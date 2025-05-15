973c514570fb62710fce7456f0c271cb475cfea3
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-05-15 00:16:05 -0700

    implement grammar

--- grammar.js
+++ grammar.js
@@ -10,8 +10,40 @@
 module.exports = grammar({
   name: "bencode",
 
+  externals: $ => [ $._string_length_prefix, $._string_value ],
+
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => $.atom,
+
+
+    atom: $ => choice(
+      $.dictionary,
+      $.list,
+      $.string,
+      $.int
+    ),
+
+    dictionary: $ => seq(
+      field('open', "d"),
+      repeat(seq($.string, $.atom)),
+      field('close', "e")
+    ),
+    list: $ => seq(
+      field('open', "l"),
+      repeat($.atom),
+      field('close', "e")
+    ),
+    // string: $ => "5:hello",
+    string: $ => seq(
+      field('length_prefix', $._string_length_prefix),
+      field('colon_separator', ":"),
+      field('value', $._string_value)
+    ),
+    int: $ => seq(
+      field('open', "i"),
+      field('value', /-?[0-9]+/),
+      field('close', "e")
+    )
   }
 });
---
+++ src/scanner.c
@@ -0,0 +1,83 @@
+#include "tree_sitter/parser.h"
+#include "tree_sitter/alloc.h"
+#include "tree_sitter/array.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+enum TokenType {
+    STRING_LENGTH_PREFIX,
+    STRING_VALUE
+};
+
+void * tree_sitter_bencode_external_scanner_create() {
+    int *len = ts_malloc(sizeof(int));
+    *len = -1;
+
+    return len;
+}
+
+void tree_sitter_bencode_external_scanner_destroy(void *payload) {
+    ts_free(payload);
+}
+
+unsigned tree_sitter_bencode_external_scanner_serialize(
+  void *payload,
+  char *buffer
+) {
+    memcpy(buffer, payload, sizeof(int));
+    return sizeof(int);
+}
+
+void tree_sitter_bencode_external_scanner_deserialize(
+  void *payload,
+  const char *buffer,
+  unsigned length
+) {
+    if (length != sizeof(int)) {
+        *(int*)payload = -1;
+        return;
+    }
+    memcpy(payload, buffer, sizeof(int));
+}
+
+bool tree_sitter_bencode_external_scanner_scan(
+  void *payload,
+  TSLexer *lexer,
+  const bool *valid_symbols
+) {
+    if (valid_symbols[STRING_LENGTH_PREFIX]) {
+        if (lexer->lookahead < 48 || lexer->lookahead > 57) {
+            // This covers EOF bc it will return 0
+            return false;
+        }
+        int len = lexer->lookahead - 48;
+        lexer->advance(lexer, false);
+        while (lexer->lookahead >= 48 && lexer->lookahead <= 57) {
+            int digit = lexer->lookahead - 48;
+            len *= 10;
+            len += digit;
+            lexer->advance(lexer, false);
+        }
+        *(int*)payload = len;
+        lexer->result_symbol = STRING_LENGTH_PREFIX;
+        return true;
+    } else if (valid_symbols[STRING_VALUE]) {
+        int len = *(int*)payload;
+        if (len >= 0) {
+            while (len > 0) {
+                if (lexer->eof(lexer)) {
+                    return false;
+                }
+                lexer->advance(lexer, false);
+                --len;
+            }
+            lexer->result_symbol = STRING_VALUE;
+            return true;
+        } else {
+            // invalid length; we should never reach this case
+            return false;
+        }
+    }
+}
